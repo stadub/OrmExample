791b3a017a9b68e76244a8c8487bff9708590c76
Dima Stadub <dima_@live.at>
2016-04-24 23:01:45 +0300

    Add EF6 porject

---
+++ OrmPresentation/Data.EF6/SuperHeroContext.cs
@@ -0,0 +1,15 @@
+﻿using Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.EF6
+{
+    internal class SuperHeroContext : DbContext
+    {
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+    }
+}
--- OrmPresentation/Data/Entity/HeroGender.cs
+++ OrmPresentation/Data/Entity/HeroGender.cs
@@ -4,10 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OrmPresentation.Entity
+namespace Data.Entity
 {
-    public class Protagonist : SuperHero
+    public enum HeroGender
     {
-        public string SuperPower { get; set; }
+        Unknown = 0, Men, Women
     }
 }
