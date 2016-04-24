b1eeb3945c1013576aa01ccf44dc7dfaff81324b
Dima Stadub <dima_@live.at>
2016-04-25 00:01:00 +0300

    repository template

---
+++ OrmPresentation/Data/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    interface IRepository<T>
+    {
+        void Add(T item);
+        void Update(T item);
+        void Remove(T item);
+        T GetById(int id);
+        IQueryable<T> Where(Expression expression);
+    }
+}
