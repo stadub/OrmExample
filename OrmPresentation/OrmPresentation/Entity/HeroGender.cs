d7834a84b99306b3a62f0ed7a25839233aea04da
Dima Stadub <dima_@live.at>
2016-04-24 22:18:07 +0300

    Added SuperHero Entities

---
+++ OrmPresentation/OrmPresentation/Entity/HeroGender.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrmPresentation.Entity
+{
+    public enum HeroGender
+    {
+        Unknown = 0, Men, Women, Alien
+    }
+}
---
+++ OrmPresentation/OrmPresentation/Entity/SuperHero.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrmPresentation.Entity
+{
+    public class SuperHero
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public HeroGender Gender { get; set; }
+    }
+
+}
